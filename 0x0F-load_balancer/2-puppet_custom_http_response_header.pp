e9915fd832b1b3fbf8b73e79004b973ac191a1bf
Mathapelo Mphahlele <modiegi.mphahlele93@gmail.com>
2023-04-12 04:05:51 -0700

    done

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,14 +3,25 @@ exec {'update':
   command => '/usr/bin/apt-get update',
 }
 -> package {'nginx':
-  ensure => 'present',
-
-# Create a custom config file for Nginx
--> file_line { 'http_header':
-  path  => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+  ensure => 'installed,
+  require => Exec['update system']
 }
--> exec {'run2':
-  command => '/usr/sbin/service nginx start',
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
