1fb4cd81c94a4a6d222abedb7dc6becdff729aea
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-05-09 05:00:05 -0700

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,6 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = r.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
