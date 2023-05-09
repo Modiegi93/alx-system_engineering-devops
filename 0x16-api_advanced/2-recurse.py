9021f9013c29438cf18ca6144108aea494488270
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-05-09 06:33:01 -0700

    task2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns a list containing
+the titles of all hot articles
+"""
+import requests as r
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
+        Gecko/20100101 Firefox/73.0"
+        }
+    param = {
+        "after": after,
+        "limit": 100,
+    }
+    response = r.get(url, headers=headers, params=param,
+                     allow_redirects=False)
+    if response.status_code == 404:
+        return None
+    else:
+        posts = response.json().get("data").get("children")
+        hot_list += [post.get("data").get("title") for post in posts]
+        after = response.json().get("data").get("after")
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        return hot_list
